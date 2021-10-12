0f34038eb459fb8435a59008d84c947dc7932ffb
frenchkebab <tubowangin@gmail.com>
2021-10-12 20:52:08 +0900

    [#10] AJAX 통신 update

--- ajax/membersAJAX.js
+++ ajax/membersAJAX.js
@@ -133,14 +133,28 @@ const membersDelete = function (index) {
 const membersUpdate = function (index) {
   const name = document.getElementsByName('members-name')[index].value;
   const age = document.getElementsByName('members-age')[index].value;
-  members[index] = { name: name, age: age };
-  membersSet();
-  return membersRead();
-};
-
-const membersSet = function () {
-  const membersSet = JSON.stringify(members);
-  sessionStorage.setItem('members', membersSet);
+  const url = 'http://localhost:3100/api/v1/members/' + index;
+  const member = {
+    name: name,
+    age: age
+  };
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function () {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      membersRead();
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      };
+      console.error(error);
+    }
+  };
+  xhrObject.open('PATCH', url);
+  xhrObject.setRequestHeader('Content-Type', 'application/json');
+  xhrObject.send(JSON.stringify(member));
 };
 
 membersRead();
