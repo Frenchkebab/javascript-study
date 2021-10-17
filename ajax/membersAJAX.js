5e9adb2541405e5c4c4689afa675a7107e7947c9
frenchkebab <tubowangin@gmail.com>
2021-10-17 19:11:11 +0900

    [#11] membersCreate 리팩토링

--- ajax/membersAJAX.js
+++ ajax/membersAJAX.js
@@ -41,45 +41,37 @@ let members;
 const membersCreate = function (form) {
   const memberNameObject = form['member-name'];
   const memberAgeObject = form['member-age'];
+
+  // Create할 member 정보 객체
   const member = {
     name: memberNameObject.value,
     age: memberAgeObject.value
   };
+
+  // 정상적으로 작동한 경우 동작할 함수
   const successFunction = function () {
     memberNameObject.value = '';
     memberAgeObject.value = '';
     membersRead();
   };
-  const xhrObject = new XMLHttpRequest();
-  xhrObject.onreadystatechange = function () {
-    if (xhrObject.readyState !== 4) return;
-    if (xhrObject.status === 200) {
-      successFunction();
-    } else {
-      const error = {
-        status: xhrObject.status,
-        statusText: xhrObject.statusText,
-        responseText: xhrObject.responseText
-      };
-      console.error(error);
-    }
-  };
-  xhrObject.open('POST', 'http://localhost:3100/api/v1/members');
-  xhrObject.setRequestHeader('Content-Type', 'application/json');
-  xhrObject.send(JSON.stringify(member));
+
+  ajax('POST', 'http://localhost:3100/api/v1/members', JSON.stringify(member), successFunction);
 };
 
 const membersRead = function () {
+  // 200 응답을 받으면 호출할 함수
   const successFunction = function (xhrObject) {
     const membersLogical = JSON.parse(xhrObject.responseText);
     members = membersLogical.members;
     const tagDivParent = document.getElementById('tag-div-parent');
     const tagDivChild = document.getElementById('tag-div-child');
     tagDivParent.innerHTML = '';
+
+    // 화면에 members 내부의 member 객체들을 표시
     for (let index in members) {
       const newDivChild = tagDivChild.cloneNode(true);
       tagDivParent.appendChild(newDivChild);
-      const membersNameObject = document.getElementsByName('members-name')[index];
+      const membersNameObject = document.getElementsByName('members-name')[index]; // 질문
       const membersAgeObject = document.getElementsByName('members-age')[index];
       const membersUpdateObject = document.getElementsByName('members-update')[index];
       const membersDeleteObject = document.getElementsByName('members-delete')[index];
@@ -90,6 +82,7 @@ const membersRead = function () {
     }
     console.log('Readed', members);
   };
+
   const xhrObject = new XMLHttpRequest();
   xhrObject.onreadystatechange = function () {
     if (xhrObject.readyState !== 4) return;
@@ -158,3 +151,23 @@ const membersUpdate = function (index) {
 };
 
 membersRead();
+
+const ajax = function (method, url, data, callback) {
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function () {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      callback(); // 성공적으로 200 응답을 받은 경우
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      };
+      console.error(error);
+    }
+  };
+  xhrObject.open(method, url);
+  xhrObject.setRequestHeader('Content-Type', 'application/json'); // 질문
+  xhrObject.send(data);
+};
